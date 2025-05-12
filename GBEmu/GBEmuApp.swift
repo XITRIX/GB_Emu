b0c9147fe94faa3a4f8c8b8cebf281ff677dfa60
Daniil Vinogradov <xitrix@bk.ru>
2025-05-12 18:58:31 +0200

    Initial Commit

---
+++ GBEmu/GBEmuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBEmuApp.swift
+//  GBEmu
+//
+//  Created by Даниил Виноградов on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GBEmuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
